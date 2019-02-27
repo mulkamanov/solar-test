c09e5be2fa3f3f4834f5593f48808f17df6f6a65
Gleb Mulkamanov <gmulkamanov@gmail.com>
2019-02-27 03:59:39 +0300

    Config validation by spec.

--- src/solar_test/config.clj
+++ src/solar_test/config.clj
@@ -1,7 +1,40 @@
 (ns solar-test.config
   (:gen-class)
   (:require [clojure.edn :as edn]
+            [clojure.java.io :as io]
+            [clojure.spec.alpha :as s]
             [mount.core :refer [defstate]]))
 
+;; Base fields.
+(s/def ::server-port int?)
+(s/def ::max-connections int?)
+(s/def ::search-timeout int?)
+(s/def ::worker-timeout int?)
+
+;; Target API fields.
+(s/def ::endpoint string?)
+(s/def ::pagesize int?)
+(s/def ::sort #{"creation"})
+(s/def ::order #{"asc" "desc"})
+(s/def ::site string?)
+(s/def ::target-api (s/keys :req-un [::endpoint ::pagesize]
+                            :opt-un [::sort ::order ::site]))
+
+(s/def ::config (s/keys :req-un [::server-port ::max-connections
+                                 ::search-timeout ::worker-timeout
+                                 ::target-api]))
+
+(defn- read-conf []
+  (->
+    (io/resource "config.edn")
+    slurp
+    edn/read-string))
+
+(defn- check-conf [conf]
+  (s/conform ::config conf))
+
 (defstate config
-  :start (edn/read-string (slurp (clojure.java.io/resource "config.edn"))))
+  :start
+  (->
+    (read-conf)
+    check-conf))
--- src/solar_test/core.clj
+++ src/solar_test/core.clj
@@ -1,8 +1,15 @@
 (ns solar-test.core
   (:gen-class)
   (:require [mount.core :as mount]
-            [solar-test.server :as server]
-            [solar-test.workers :as workers]))
+            [solar-test.server :as server]))
+
+(defn- start []
+  (mount/start)
+  :started)
+
+(defn- stop []
+  (mount/stop)
+  :stoped)
 
 (defn -main [& args]
   (mount/start)
